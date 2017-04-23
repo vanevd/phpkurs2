e8874af58a842971948d5233b8f13288345a7a43
YanaTsoneva <y.tsoneva@gmail.com>
2017-04-23 17:39:03 +0300

    test

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
-Hello
+<html>
+    <head>
+        <META http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Test title</title>
+        <link href="/css/style.css" rel="stylesheet" type="text/css"/>
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+<p>Иван </p>
+<p class="client">Петров</p>
+<p class="client1">Иванов</p>
+
+        <table border="2">
+            <tr>
+                <td> Иван </td>
+                <td> Петров </td>
+                <td class="client"> 0889 405 904 </td>
+                <td> ivan.petrov@abv.bg </td>
+            </tr>
+            <tr>
+                <td> Христо </td>
+                <td> Иванов </td>
+                <td> 0876 634 345 </td>
+                <td> hristo.ivanov@abv.bg </td>
+            </tr>
+        </table>
+
+        <div class="client">
+            Димитър Иванов
+        </div>
+        <div class="client">
+            Димитър Иванов
+        </div>
+        <div class="client">
+            Димитър Иванов
+        </div>
+        <div class="client">
+            Димитър Иванов
+        </div>
+        <div class="client1">
+            Петър Георгиев
+        </div>
+
+    </body>
+</html>
