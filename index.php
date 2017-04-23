0120f1354d74f049ca69c41be5ff15dd1c7fa4fb
Dimitar Vanev <radibase@gmail.com>
2017-04-23 17:34:55 +0300

    test

--- index.php
+++ index.php
@@ -1,1 +1,77 @@
-Hello
+<html>
+<head>
+  <META http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Test</title>
+  <link href="/css/style.css" rel="stylesheet" type="text/css"/>
+  <style>
+      
+  </style>    
+</head>
+
+<body>
+<p class="client">    
+Ivan<br>
+Petrov<br>
+Ivanov<br>
+</p>
+<p class="client1">    
+Ivan<br>
+Petrov<br>
+Ivanov<br>
+</p>
+<p>
+Иван Петров<br>
+</p>
+
+<table border="1" style="" class="client2">
+    <tr>
+        <td class="client">Ivan</td>
+        <td>Petrov</td>
+        <td>0878150111</td>
+        <td>ivan@abv.bg</td>
+    </tr>    
+    <tr>
+        <td>Hristo</td>
+        <td>Ivanov</td>
+        <td>0878150111</td>
+        <td>ivan@abv.bg</td>
+    </tr>    
+    
+</table>
+
+<div class="client">
+Dimitar Ivanov 1   
+</div>    
+<div class="client">
+Dimitar Ivanov 2   
+</div>    
+<div class="client">
+Dimitar Ivanov 3   
+</div>    
+<div class="client">
+Dimitar Ivanov 4   
+</div>    
+<div class="client">
+Dimitar Ivanov 4   
+</div>    
+<div class="client">
+Dimitar Ivanov 4   
+</div>    
+<div class="client">
+Dimitar Ivanov 4   
+</div>    
+<div class="client">
+Dimitar Ivanov 4   
+</div>    
+<div class="client">
+Dimitar Ivanov 4   
+</div>    
+<div class="client">
+Dimitar Ivanov 4   
+</div>    
+
+<div class="client1">Petar Petrov</div>    
+
+</body>      
+
+</html>    
