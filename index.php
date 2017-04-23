46e056f5d8a1aa511c6148fb08001a0658540874
Marietanikolova <marietanikolova@mail.bg>
2017-04-23 17:49:08 +0300

    test

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-Hello
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title>Test</title>
+<link href="css/style.css rel="stylesheet" type="text/css"/>
+// насочва към файла за дизайн
+<style>
+// дава дизайн за конкретната страница
+</style>
+</head>
+<header>
+</header>
+<body>
+<p class =''client">
+Ivan<br>
+Petrov<br>
+Ivanov<br>
+</p>
+Иван<br>
+Петров<br>
+Иванов<br>
+<br>
+<br>
+<table border=''1'' style="" class="client">
+<tr>
+<td>Ivan</td>
+<td>0888564052</td>
+<td>ivan@mail.bg</td>
+
+</tr>
+<div>
+Ivan
+</div>
+
+
+<div class=''client''>
+</div>
+</body>
+</html>
